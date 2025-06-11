9d693ebf0e4727c6ac4c665acc123b8e012de19c
m-eleoterio <m.eleoteriovieira@gmail.com>
2025-06-10 23:00:53 -0300

    feat: layout content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Inter, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import Image from "next/image";
+import { ThemeToggle } from "@/components/ui/theme-toggle";
+import { Sider } from "@/components/sider/sider";
+import { MenuTopbar } from "@/components/menu-topbar/menu-topbar";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -29,16 +32,23 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${geistMono.variable} antialiased max-w-screen overflow-hidden`}
       >
-        <div className="bg-black/50 w-screen h-screen fixed top-0 left-0 backdrop-blur-md z-[-1]" />
-        <Image
-          width={1920}
-          height={1080}
-          src="/background.jpg"
-          alt="FocusMood"
-          className="backdrop-blur-md h-screen w-screen fixed top-0 left-0 z-[-2]"
-        />
         <ThemeProvider attribute="class" defaultTheme="system">
-          {children}
+          <div className="bg-black/50 w-screen h-screen fixed top-0 left-0 backdrop-blur-md z-[-1]" />
+          <Image
+            width={1920}
+            height={1080}
+            src="/background.jpg"
+            alt="FocusMood"
+            className="backdrop-blur-md h-screen w-screen fixed top-0 left-0 z-[-2]"
+          />
+          <div className="flex p-10 justify-between w-full">
+            <MenuTopbar />
+            <ThemeToggle />
+          </div>
+          <div className="flex gap-10">
+            <Sider />
+            {children}
+          </div>
         </ThemeProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import { ThemeToggle } from "@/components/ui/theme-toggle";
-
 export default function Home() {
   return (
-    <div>
-      <div className="fixed top-10 right-10">
-        <ThemeToggle />
-      </div>
-    </div>
+    <>
+      <h1>hello world</h1>
+    </>
   );
 }
